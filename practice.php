2992972226f9c7754b3315a075cf4e0075980389
sarah <saraheunjikim@gmail.com>
2020-11-28 20:19:19 -0600

    Delete practice.php

--- practice.php
+++
@@ -1,3 +0,0 @@
-Array ( [1] => Array ([bookId] => 1 [title] => The Royal Easter [price] => 14.99 [quantityInput] => 2 )
-        [14] => Array ([bookId] => 14 [title] => The Best Mexican Recipes [price] => 17.11 [quantityInput] => 4 )
-       )
